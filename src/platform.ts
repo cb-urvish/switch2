52f9ee04181e7caf8fcbb275e54b15c578f7e567
cb-urvish <urvish.r@chipbucket.com>
2023-10-21 18:15:24 +0530

    final fix

--- src/platform.ts
+++ src/platform.ts
@@ -59,10 +59,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         exampleUniqueId: 'ABCD',
         exampleDisplayName: 'Bedroom',
       },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
+      // {
+      //   exampleUniqueId: 'EFGH',
+      //   exampleDisplayName: 'Kitchen',
+      // },
     ];
 
     // loop over the discovered devices and register each one if it has not already been registered
